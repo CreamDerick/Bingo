4260c182369dd273dc0b881036d717f63445c0f1
CreamDerick <derickrobles1512@gmail.com>
2024-03-20 11:27:58 +0800

    5th commit

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ let isB = false,
     isG = false,
     isO = false;
 
+let BNumber,
+    INumber,
+    NNumber,
+    GNumber,
+    ONumber;
+
 function generateNumber(max) {
     //add = +
     //sub = -
@@ -18,22 +24,27 @@ function generateNumber(max) {
 
     //GT >, GTE > =, LT <, LTE < =, EQ == NEQ ! =, AND &&, OR ||
     if (randomNumber <= 15) {
+        BNumber = randomNumber;
         isB = true;
-        console.log(`The number is in B.`)
+        console.log(`The number ${randomNumber} is in B.`)
     } 
     else if (randomNumber >= 16 && randomNumber <= 30) {
+        INumber = randomNumber;
         isI = true;
-        console.log(`The number is in I.`)
+        console.log(`The number ${randomNumber} is in I.`)
     }
     else if (randomNumber >= 31 && randomNumber <= 45) {
+        NNumber = randomNumber;
         isN = true;
-        console.log(`The number is in N.`)
+        console.log(`The number ${randomNumber} is in N.`)
     }
     else if (randomNumber >= 46 && randomNumber <= 60) {
+        GNumber = randomNumber;
         isG = true;
-        console.log(`The number is in G.`)
+        console.log(`The number ${randomNumber} is in G.`)
     }
     else if (randomNumber >= 61 && randomNumber <= 75) {
+        ONumber = randomNumber;
         isO = true;
         console.log(`The number is in O.`)
     }
@@ -42,9 +53,26 @@ function generateNumber(max) {
     }
 
     if (isB == true && isI == true && isN == true && isG == true && isO == true) {
-        console.log("nice");
+        console.log("BINGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        console.table ({
+            B: BNumber,
+            I: INumber,
+            N: NNumber,
+            G: GNumber,
+            O: ONumber
+        })
     }
 
     
     return console.log(randomNumber);
+}
+//let (counter >0)
+while (
+isB == false||
+isI == false||
+isN == false||
+isG == false||
+isO == false
+) {
+    generateNumber(75);
 }
